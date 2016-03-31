5b9ae7a689b4b228131c10eec89e1af01ac18bbb
Tim Hagemann <tim@infinityblade.de>
2016-03-31 15:29:04 +0200

    Added first steps in second exercise.

---
+++ praktikum/praktikum_2.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/swipl
+:- consult('praktikum_1.pl').
+
+lex(wer, ip).
+lex(was, ip).
+lex(wessen, ip).
+lex(wen, ip).
+lex(wem, ip).
+lex(welcher, ip).
+
+lex(ist, v).
+lex(hat, v).
+lex(gehoert, v).
+lex(verheiratet, v).
+
+lex(der, a).
+lex(die, a).
+lex(das, a).
+
+lex(onkel, n).
+lex(tante, n).
+lex(tochter, n).
+lex(sohn, n).
+lex(vater, n).
+lex(mutter, n).
+lex(geschwister, n).
+lex(oma, n).
+lex(opa, n).
+lex(frau, n).
+lex(mann, n).
+
+lex(von, pr).
+lex(mit, pr).
+
+lex(X, en) :- maenlich(X); weiblich(X).
+
+
+s --> ip, vp.
+ip --> [X], { lex(X, ip) }.
+v --> [X], { lex(X, v) }.
+vp --> v.
+vp --> v,np.
+np --> en.
+np --> a,n.
+np --> a,n,pp.
+en --> [X], { lex(X, en) }.
+n --> [X], { lex(X, n) }.
+a --> [X], { lex(X, a) }.
+pp --> p, np.
+p --> [X], { lex(X, pr) }.
+
+
