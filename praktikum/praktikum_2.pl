eb0181c0de7d7ed03efe6c1be3ecf081c8317051
Simon K <Oct4v14n@googlemail.com>
2016-04-07 15:18:05 +0200

    Add causus.

--- praktikum/praktikum_2.pl
+++ praktikum/praktikum_2.pl
@@ -8,22 +8,16 @@ lex(von, pr).
 lex(X, en, m) :- maennlich(X).
 lex(X, en, f) :- weiblich(X).
 
-lex(wer, ip, m).
-lex(wer, ip, f).
-lex(was, ip, n).
-lex(wessen, ip, _).
-lex(wen, ip, m).
-lex(wen, ip, f).
-lex(wem, ip, m).
-lex(welcher, ip, m).
-
 lex(ist, v, sg).
 lex(sind, v, pl).
 
-lex(der, a, m, sg).
-lex(die, a, f, sg).
-lex(die, a, _, pl).
-lex(das, a, n, sg).
+lex(wer, ip, m, no).
+lex(wer, ip, f, no).
+lex(was, ip, n, _).
+lex(wen, ip, m, ak).
+lex(wen, ip, f, ak).
+lex(wem, ip, m, da).
+lex(wem, ip, f, da).
 
 % Ueberprueft ob es eine 2-Stellige Funktion gibt, die wie X heisst (bruder, mutter, onkel, ...)
 % Das Cut('!') ist wichtig, damit es keine doppel-Ausgaben gibt.
@@ -43,6 +37,11 @@ lex(sohn, n, m, sg).
 lex(neffe, n, m, sg).
 lex(nichte, n, f, sg).
 lex(eltern, n, f, pl).
+
+lex(der, a, m, sg, no).
+lex(die, a, f, sg, no).
+lex(die, a, _, pl, _).
+lex(das, a, n, sg, no).
 
 s :-
 	read_sentence(S),
@@ -71,7 +70,7 @@ write_list([Name]) :- write(Name).
 write_list([Name|Rest]) :- Rest \= [], write(Name), write(' und '), write_list(Rest).
 
 % wer ist der onkel von jeff
-s(F) --> ip(G1), vp(B, G1, _N1), pp(P, _G2, _N2), [?], { F =.. [B, _X, P] }.
+s(F) --> ip(G1), vp(B, G1, _N1, K), pp(P, _G2, _N2, K), [?], { F =.. [B, _X, P] }.
 % von wem ist corinna die schwester
 % von = präposition
 % wem = Interrogativpronomen
@@ -79,19 +78,19 @@ s(F) --> ip(G1), vp(B, G1, _N1), pp(P, _G2, _N2), [?], { F =.. [B, _X, P] }.
 % corinna = eignename
 % die = artikel
 % schwester = nomen
-s(F) --> p, ip(G), vp(P, G, N), np(B, _, N), [?], { F =.. [B, P, _X] }.
+s(F) --> p, ip(G), vp(P, G, N, K), np(B, _, N, K), [?], { F =.. [B, P, _X] }.
 % ist hannes der onkel von jeff
-s(F) --> vp(P1, G, N), np(B, G, N), pp(P2, _, _), [?], {F =.. [B, P1, P2]}.
+s(F) --> vp(P1, G, N, K), np(B, G, N, K), pp(P2, _, _, K), [?], {F =.. [B, P1, P2]}.
 ip(G) --> [X], { lex(X, ip, G) }.
 v(N) --> [X], { lex(X, v, N) }.
-vp(X, G, N) --> v(N), np(X, G, N).
-vp(_, _, N) --> v(N).
-np(X, G, sg) --> en(X, G).
-np(X, G, N) --> a(G, N),n(X, G, N).
-%np(X, G, N) --> a(G, N),n(X, G, N),pp(_, _, N).
+vp(X, G, N, K) --> v(N), np(X, G, N, K).
+vp(_, _, N, _) --> v(N).
+np(X, G, sg, _) --> en(X, G).
+np(X, G, N, K) --> a(G, N, K),n(X, G, N).
+%np(X, G, N, K) --> a(G, N, K),n(X, G, N),pp(_, _, N, K).
 en(X, G) --> [X], { lex(X, en, G) }.
 n(X, G, N) --> [X], { lex(X, n, G, N) }.
-a(G, N) --> [X], { lex(X, a, G, N) }.
-pp(X, G, N) --> p, np(X, G, N).
+a(G, N, K) --> [X], { lex(X, a, G, N, K) }.
+pp(X, G, N, K) --> p, np(X, G, N, K).
 p --> [X], { lex(X, pr) }.
 
