223e366b71549751647fa8e916331fa91d5d63ec
Tim Hagemann <kumpelblase2@users.noreply.github.com>
2016-04-13 14:55:28 +0200

    Initial support for plural questions
    This now works:
    1 ?- s.
    > wer sind die brueder von corinna?
    der bruder von corinna ist michael.
    true .
    
    2 ?- s.
    > wer sind die halbgeschwister von corinna?
    die halbgeschwister von corinna sind jeff und tina und lukas.

--- praktikum/praktikum_2.pl
+++ praktikum/praktikum_2.pl
@@ -19,24 +19,36 @@ lex(was, ip, n, _).
 lex(wem, ip, m, da).
 lex(wem, ip, f, da).
 
-% Ueberprueft ob es eine 2-Stellige Funktion gibt, die wie X heisst (bruder, mutter, onkel, ...)
-% Das Cut('!') ist wichtig, damit es keine doppel-Ausgaben gibt.
-%lex(X, n) :- Y =.. [X, _A, _B], Y, !.
-lex(bruder, n, m, sg).
-lex(schwester, n, f, sg).
-lex(opa, n, m, sg).
-lex(oma, n, f, sg).
-lex(vater, n, m, sg).
-lex(mutter, n, f, sg).
-lex(halbbruder, n, m, sg).
-lex(halbschwester, n, f, sg).
-lex(onkel, n, m, sg).
-lex(tante, n, f, sg).
-lex(tochter, n, f, sg).
-lex(sohn, n, m, sg).
-lex(neffe, n, m, sg).
-lex(nichte, n, f, sg).
-lex(eltern, n, f, pl).
+lex(bruder, bruder, n, m, sg).
+lex(brueder, bruder, n, m, pl).
+lex(schwester, schwester, n, f, sg).
+lex(schwestern, schwester, n, f, pl).
+lex(opa, opa, n, m, sg).
+lex(opas, opa, n, m, pl).
+lex(oma, oma, n, f, sg).
+lex(oma, omas, n, f, pl).
+lex(vater, vater, n, m, sg).
+lex(vaeter, vater, n, m, pl).
+lex(mutter, mutter, n, f, sg).
+lex(muetter, mutter, n, f, pl).
+lex(halbbruder, halbbruder, n, m, sg).
+lex(halbbrueder, halbbruder, n, m, pl).
+lex(halbschwester, halbschwester, n, f, sg).
+lex(halbschwestern, halbschwester, n, f, pl).
+lex(halbgeschwister, halbgeschwister, n, f, pl).
+lex(onkel, onkel, n, m, sg).
+lex(onkel, onkel, n, m, pl).
+lex(tante, tante, n, f, sg).
+lex(tanten, tante, n, f, pl).
+lex(tochter, tochter, n, f, sg).
+lex(toechter, tochter, n, f, pl).
+lex(sohn, sohn, n, m, sg).
+lex(soehne, sohn, n, m, pl).
+lex(neffe, neffe, n, m, sg).
+lex(neffen, neffe, n, m, pl).
+lex(nichte, nichte, n, f, sg).
+lex(nichten, nichte, n, f, pl).
+lex(eltern, eltern, n, f, pl).
 
 lex(der, a, m, sg, no).
 lex(die, a, f, sg, no).
@@ -45,7 +57,7 @@ lex(das, a, n, sg, no).
 
 s :-
 	read_sentence(S),
-	%S = [ist, corinna, der, bruder, von, michael, ?],
+	%S = [von, wem, ist, hannes, der,bruder, ?],
 	(
 		(s(X, S, []), !, answer(X));
 		write('du sprechen deutsch?')
@@ -54,7 +66,7 @@ s :-
 answer(X) :-
 	X =.. F,
 	[BEZIEHUNG, P1, P2] = F,
-	(lex(BEZIEHUNG, n, Gender, Numerus); !, write('die beziehung '), write(BEZIEHUNG), write(' wurde nicht gefunden.'), fail),
+	(lex(BEZIEHUNG, _, n, Gender, Numerus); !, write('die beziehung '), write(BEZIEHUNG), write(' wurde nicht gefunden.'), fail),
 	(lex(Artikel, a, Gender, Numerus, no); !, write('der artikel '), write(Artikel), write(' wurde nicht gefunden.'), fail),
 	(lex(Verb, v, Numerus); !, write('das verb '), write(Verb), write(' wurde nicht gefunden.'), fail),
 	((
@@ -79,7 +91,7 @@ write_list([Name|Rest]) :- Rest \= [], write(Name), write(' und '), write_list(R
 % wer ist der onkel von jeff
 s(F) --> ip(G1, K), vp(B, G1, _N1, K), pp(P, _G2, _N2, K), [?], { F =.. [B, _X, P] }.
 % von wem ist corinna die schwester
-% von = präposition
+% von = praeposition
 % wem = Interrogativpronomen
 % ist = verb
 % corinna = eignename
@@ -91,13 +103,12 @@ s(F) --> v(N), np(P1, G, N, K), np(B, G2, N, K), pp(P2, _, _, K), [?], {F =.. [B
 ip(G, K) --> [X], { lex(X, ip, G, K) }.
 v(N) --> [X], { lex(X, v, N) }.
 vp(X, G, N, K) --> v(N), np(X, G, N, K).
-%vp(_, _, N, _) --> v(N).
+vp(_, _, N, _) --> v(N).
 np(X, G, sg, _) --> en(X, G).
 np(X, G, N, K) --> a(G, N, K),n(X, G, N).
 %np(X, G, N, K) --> a(G, N, K),n(X, G, N),pp(_, _, N, K).
 en(X, G) --> [X], { lex(X, en, G) }.
-n(X, G, N) --> [X], { lex(X, n, G, N) }.
+n(B, G, N) --> [X], { lex(X, B, n, G, N) }.
 a(G, N, K) --> [X], { lex(X, a, G, N, K) }.
 pp(X, G, N, K) --> p, np(X, G, N, K).
 p --> [X], { lex(X, pr) }.
-
