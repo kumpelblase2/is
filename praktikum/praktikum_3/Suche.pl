93e824c6b08479810e989f2b5db6d881cfa23555
Tim Hagemann <tim@infinityblade.de>
2016-05-06 08:44:48 +0200

    Added files for third exercise.

---
+++ praktikum/praktikum_3/Suche.pl
@@ -0,0 +1,17 @@
+
+
+
+:- consult('Suche_Modul_Allgemein.pl').
+
+
+:- consult('Suche_Modul_Informierte_Suche.pl').
+
+
+
+%%% Spezieller Teil: Wassergef��e
+%:- consult('Suche_Modul_Wasser.pl').
+
+
+
+%%% Spezieller Teil: Planung
+:- consult('Suche_Modul_Planung.pl').
