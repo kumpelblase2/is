3592fd4665ec6b3c5d68ac7d7f22b8546f2a3c23
Tim Hagemann <tim@infinityblade.de>
2016-05-13 09:51:21 +0200

    Somewhat progress. Doesn't give a proper ressult though. Maybe missing some constraints.

--- praktikum/praktikum_4/src/Contraints.scala
+++ praktikum/praktikum_4/src/Contraints.scala
@@ -1,59 +1,61 @@
-sealed trait Variable
+sealed trait Variable {
+  val index: Int
+}
 
 trait VariableCollection[T] {
   def values : List[T]
 }
 
-sealed trait Person extends Variable
-object German extends Person
-object Swede extends Person
-object Brit extends Person
-object Dane extends Person
-object Norwegian extends Person
+abstract class Person(val index: Int) extends Variable
+object German extends Person(1)
+object Swede extends Person(2)
+object Brit extends Person(3)
+object Dane extends Person(4)
+object Norwegian extends Person(5)
 
 object Person extends VariableCollection[Person] {
   val values = List(German, Swede, Brit, Dane, Norwegian)
 }
 
-sealed trait Color extends Variable
-object Red extends Color
-object Blue extends Color
-object Green extends Color
-object White extends Color
-object Yellow extends Color
+abstract class Color(val index: Int) extends Variable
+object Red extends Color(6)
+object Blue extends Color(7)
+object Green extends Color(8)
+object White extends Color(9)
+object Yellow extends Color(10)
 
 object Color extends VariableCollection[Color] {
   val values = List(Red, Blue, Green, White, Yellow)
 }
 
-sealed trait Drink extends Variable
-object Water extends Drink
-object Tee extends Drink
-object Coffee extends Drink
-object Milk extends Drink
-object Beer extends Drink
+abstract class Drink(val index: Int) extends Variable
+object Water extends Drink(11)
+object Tee extends Drink(12)
+object Coffee extends Drink(13)
+object Milk extends Drink(14)
+object Beer extends Drink(15)
 
 object Drink extends VariableCollection[Drink] {
   val values = List(Water, Tee, Coffee, Milk, Beer)
 }
 
-sealed trait Cigarette extends Variable
-object Dunhill extends Cigarette
-object PallMall extends Cigarette
-object Winfield extends Cigarette
-object Rothmanns extends Cigarette
-object Malboro extends Cigarette
+abstract class Cigarette(val index: Int) extends Variable
+object Dunhill extends Cigarette(16)
+object PallMall extends Cigarette(17)
+object Winfield extends Cigarette(18)
+object Rothmanns extends Cigarette(19)
+object Malboro extends Cigarette(20)
 
 object Cigarette extends VariableCollection[Cigarette] {
   val values = List(Dunhill, PallMall, Winfield, Rothmanns, Malboro)
 }
 
-sealed trait Pet extends Variable
-object Dog extends Pet
-object Bird extends Pet
-object Horse extends Pet
-object Cat extends Pet
-object Fish extends Pet
+abstract class Pet(val index: Int) extends Variable
+object Dog extends Pet(21)
+object Bird extends Pet(22)
+object Horse extends Pet(23)
+object Cat extends Pet(24)
+object Fish extends Pet(25)
 
 object Pet extends VariableCollection[Pet] {
   val values = List(Dog, Bird, Horse, Cat, Fish)
