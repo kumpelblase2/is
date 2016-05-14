f99ac8ebd737e0bfcb3191bd69a7f58ac885003f
Tim Hagemann <tim@infinityblade.de>
2016-05-15 00:28:09 +0200

    Some comments

--- praktikum/praktikum_4/src/CSP.scala
+++ praktikum/praktikum_4/src/CSP.scala
@@ -1,53 +1,85 @@
 class CSP[Var, Opt] {
   type Constraint = (Opt, Set[Opt]) => Boolean
-  var variables: List[Var] = List()
-  var domain: Map[Var, Set[Opt]] = Map.empty
-  var constraints: List[(Var, Var, Constraint)] = List()
+  var variables: List[Var] = List() // All variables that we have
+  var domain: Map[Var, Set[Opt]] = Map.empty // All possible values for the variables
+  var constraints: List[(Var, Var, Constraint)] = List() // All constraints / edges
 
+  /**
+    * Adds a variables to the graph and set the possibles values
+    *
+    * @param variable Variable
+    * @param values   Possible values
+    */
   def addVar(variable: Var, values: Set[Opt]): Unit = {
     variables = variable +: variables
     domain = domain.updated(variable, values)
   }
 
+  /**
+    * Add a Bi-Directional constraint. This is equal to calling #{addConstraint} twice with different order of variables.
+    *
+    * @param variable   Source variable
+    * @param variable2  Target variable
+    * @param constraint Constraint
+    */
   def addBiConstraint(variable: Var, variable2: Var, constraint: Constraint): Unit = {
     constraints = constraints ++ List((variable, variable2, constraint), (variable2, variable, constraint))
   }
 
+  /**
+    * Adds a constraint to the graph.
+    *
+    * @param variable   Source variable
+    * @param variable2  Target variable
+    * @param constraint Constraint
+    */
   def addConstraint(variable: Var, variable2: Var, constraint: Constraint): Unit = {
     constraints = constraints ++ List((variable, variable2, constraint))
   }
 
-  def addBiConstraint(variables: List[Var], constraint: Constraint): Unit = {
+  /**
+    * Adds a Bi-Directional constraint between every variable in the sequence.
+    *
+    * @param variables  All variables
+    * @param constraint Constraint
+    */
+  def addBiConstraint(variables: Traversable[Var], constraint: Constraint): Unit = {
     variables.foreach { variable1 =>
       variables.foreach { variable2 =>
-        if(variable1 != variable2) {
+        if(variable1 != variable2) { // Only add it if the variables are not the same
           this.addConstraint(variable1, variable2, constraint)
         }
       }
     }
   }
 
+  /**
+    * Revises the given edge and updates the domain if any changes are necessary
+    *
+    * @param edge The edge to check
+    * @return     true of false depending if updates were made
+    */
   def revise(edge: (Var, Var, Constraint)) = {
     val (source, target, constraint) = edge
     val sourceValues = domain(source)
     val targetValues = domain(target)
-    val newDomainValues = sourceValues.filter { value =>
+    val newDomainValues = sourceValues.filter { value => // Filter out invalid values
       constraint(value, targetValues)
     }
 
-    domain = domain.updated(source, newDomainValues)
+    domain = domain.updated(source, newDomainValues) // Update domain with new values
     newDomainValues.size != sourceValues.size
   }
 
   private def ac3(): Unit = {
-    var q = constraints.filter(_ => true)
+    var q = constraints.filter(_ => true) // Just create a copy
     while(q.nonEmpty) {
       val current = q.head
       q = q.tail
       if(revise(current)) {
-        q = constraints.filter { edge =>
+        q = (q ++ constraints.filter { edge => // Get all constraints that don't have the same target node
           indexOfVariable(current._2) != indexOfVariable(edge._1)
-        }
+        }).distinct
       }
     }
   }
@@ -59,7 +91,7 @@ class CSP[Var, Opt] {
       val current = q.head
       q = q.tail
       if (revise(current)) {
-        q = following(indexOfVariable(current._1), cv)
+        q = following(indexOfVariable(current._1), cv) // Get all the constraints that follow after this variable
         val k = current._1
         consistent = domain(k).nonEmpty
       }
@@ -69,7 +101,7 @@ class CSP[Var, Opt] {
   }
 
   private def init(cv: Int) = {
-    constraints.filter { edge =>
+    constraints.filter { edge => // Create the initial queue for the ac-3 LA
       indexOfVariable(edge._1) > cv && indexOfVariable(edge._2) == cv
     }
   }
@@ -84,33 +116,43 @@ class CSP[Var, Opt] {
   private def solveRecursive(cv: Int): Boolean = {
     val variable = variables(cv)
     val currDomain = domain(variable)
-    if(currDomain.isEmpty)
+    if(currDomain.isEmpty) // If we don't have any possibilities, we're done
       return false
 
     val oldDomain = domain
-    domain = domain.updated(variable, Set(currDomain.head))
-    if(ac3LA(cv)) {
-      if(cv == variables.size - 1)
+    domain = domain.updated(variable, Set(currDomain.head)) // Just try one value
+    if(ac3LA(cv)) { // Check if the graph stays consistent with this
+      if(cv == variables.size - 1) // if we reached the last variable too, we're finished and it's solved
         return true
 
-      if(solveRecursive(cv + 1))
+      if(solveRecursive(cv + 1)) // Recursive call for next variable
         return true
     }
 
-    domain = oldDomain
-    domain = domain.updated(variable, currDomain.tail)
-    solveRecursive(cv)
+    domain = oldDomain // Restore old domain
+    domain = domain.updated(variable, currDomain.tail) // Since the chose value didn't work, remove it
+    solveRecursive(cv) // try again with another variable
   }
 
+  /**
+    * Try to solve things.
+    *
+    * @return true or false depending if it succeeded or not
+    */
   def solve(): Boolean = {
-    ac3()
-    solveRecursive(0)
+    ac3() // Run once to make sure everything is consistent and all constraints are met
+    solveRecursive(0) // Start recursion with trying out values
   }
 
   private def indexOfVariable(variable: Var) = {
     variables.indexOf(variable)
   }
 
+  /**
+    * Gets the solution as a String
+    *
+    * @return Solution string
+    */
   def solution: String = {
     domain.map { entry =>
       val (variable, value) = entry
--- praktikum/praktikum_4/src/Main.scala
+++ praktikum/praktikum_4/src/Main.scala
@@ -1,6 +1,6 @@
 object Main {
   val allVars: List[Variable] = Color.values ++ Pet.values ++ Drink.values ++ Person.values ++ Cigarette.values // Create a list of all variables that are going to be used
-  val within = (diff: Int) => (value: Int, value2: Int) => Math.abs(value - value2) == diff //
+  val within = (diff: Int) => (value: Int, value2: Int) => Math.abs(value - value2) == diff //Helper for doing within(2) => within 2 houses
 
   def main(args: Array[String]) {
     val csp = new CSP[Variable, Int]
@@ -14,9 +14,10 @@ object Main {
     val rightTo = (var1: Int, var2: Set[Int]) => var2.exists(var1 > _)
     val nextTo = (var1: Int, var2: Set[Int]) => var2.exists(within(1)(var1, _))
 
-    csp.addConstraint(Milk, Milk, (var1, var2) => var1 == 3)
-    csp.addConstraint(Norwegian, Norwegian, (var1, var2) => var1 == 1)
+    csp.addConstraint(Milk, Milk, (var1, var2) => var1 == 3) // We know milk is in the third house
+    csp.addConstraint(Norwegian, Norwegian, (var1, var2) => var1 == 1) // we know the norwegian lives in the first house
 
+    // Other clues
     csp.addBiConstraint(Brit, Red, same)
     csp.addBiConstraint(Swede, Dog, same)
     csp.addBiConstraint(Dane, Tee, same)
@@ -38,7 +39,7 @@ object Main {
     csp.addBiConstraint(Cigarette.values, notSame)
     csp.addBiConstraint(Person.values, notSame)
 
-    println(csp.solve())
+    println(csp.solve()) // GET ON WITH IT
     println(csp.solution)
   }
 }
